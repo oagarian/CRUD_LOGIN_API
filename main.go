226a5d47bab6d551db545dde767c8cbcd9ba60a7
oagarian <tns6@aluno.ifal.edu.br>
2023-03-11 11:16:47 -0300

    Adding functions

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"fmt"
+	"reflect"
+	"strings"
 )
 
 func handler(c *fiber.Ctx) error {
@@ -10,10 +12,47 @@ func handler(c *fiber.Ctx) error {
 	return nil
 }
 
-type account struct {
-	User string 
-	Password string 
-	Email string 
+type Account struct {
+	User string `json:"User"`
+	Email string `json:"Email"`
+	Password string `json:"Password"`
+}
+
+var Users []Account
+
+func logon(user, email, password string) {
+	newUser := Account{
+		User: user, 
+		Email: email, 
+		Password: password,
+	}
+	Users = append(Users, newUser)
+}
+
+func verifyUser(user, email, password string) bool {
+	for _, u := range Users {
+		values := reflect.ValueOf(u)
+		typesOf := values.Type()
+		for i := 0; i < values.NumField(); i++ {
+			if (strings.EqualFold(typesOf.Field(i).Name, "User") || strings.EqualFold(typesOf.Field(i).Name, "Email")) {
+				valueOfFields := fmt.Sprintf("%s", values.Field(i).Interface())
+				var valueOfPassword string
+				if (strings.EqualFold(typesOf.Field(i).Name, "Password")) {
+					valueOfPassword = fmt.Sprintf("%s", values.Field(i).Interface())
+				}
+				if(!strings.EqualFold(valueOfFields, user)){
+					if(valueOfPassword == password) {
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
+		}
+
+	}
+	return true;
+	
 }
 
 func main() {
