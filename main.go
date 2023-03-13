1af23c61c0d6d9b8995eab7de47e482cc91dfbbe
oagarian <tns6@aluno.ifal.edu.br>
2023-03-13 18:11:12 -0300

    Add register system

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"fmt"
-	"reflect"
-	"strings"
+    "log"
+	"github.com/gofiber/fiber/v2"
+
 )
 
 func handler(c *fiber.Ctx) error {
+	c.Response().Header.Set("Content-Type", "application/json")
+	
 	fmt.Fprintf(c, "Arroba")
 	return nil
 }
 
 type Account struct {
-	User string `json:"User"`
-	Email string `json:"Email"`
-	Password string `json:"Password"`
+	User string `json:"user"`
+	Email string `json:"email"`
+	Password string `json:"password"`
 }
 
 var Users []Account
@@ -27,36 +29,60 @@ func logon(user, email, password string) {
 		Password: password,
 	}
 	Users = append(Users, newUser)
+	fmt.Println("Logon sucessfuly!")
 }
 
-func verifyUser(user, email, password string) bool {
-	for _, u := range Users {
-		values := reflect.ValueOf(u)
-		typesOf := values.Type()
-		for i := 0; i < values.NumField(); i++ {
-			if (strings.EqualFold(typesOf.Field(i).Name, "User") || strings.EqualFold(typesOf.Field(i).Name, "Email")) {
-				valueOfFields := fmt.Sprintf("%s", values.Field(i).Interface())
-				var valueOfPassword string
-				if (strings.EqualFold(typesOf.Field(i).Name, "Password")) {
-					valueOfPassword = fmt.Sprintf("%s", values.Field(i).Interface())
-				}
-				if(!strings.EqualFold(valueOfFields, user)){
-					if(valueOfPassword == password) {
-						return false;
-					}
-				}
-			} else {
-				return false;
-			}
-		}
+func register(c *fiber.Ctx) error {
+	c.Response().Header.Set("Content-Type", "application/json")
+	payload := struct {
+        User  string 
+        Email string 
+		Password string 
+    }{}
 
+	if err := c.BodyParser(&payload); err != nil {
+		log.Fatal(err)
 	}
-	return true;
+
+	logon(payload.User, payload.Email, payload.Password)
+	fmt.Println(payload.User)
+	fmt.Println(payload.Email)
+	fmt.Println(payload.Password)
+
+	return nil
 	
 }
+
+
+func verifyUser(user, email, password string) bool {
+	
+}
+
+func login(c *fiber.Ctx) error {
+	c.Response().Header.Set("Content-Type", "application/json")
+	payload := struct {
+        User  string 
+        Email string 
+		Password string 
+    }{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		log.Fatal(err)
+	}
+
+	if (verifyUser(payload.User, payload.Email, payload.Password)) {
+		fmt.Println("Login sucessfuly! - pt2")
+	} else {
+		fmt.Println("Login failed!")
+	}
+	return nil
+}
+
 
 func main() {
 	app := fiber.New()
 	app.Get("/", handler)
+	app.Post("/register", register)
+	app.Get("/login")
 	app.Listen(":8080")
 }
